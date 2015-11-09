84d5965bd41390d44ad386e1dd29f99d23172722
Stefan <stefan@neuland.io>
2015-11-09 22:08:13 +0100

    add profile app

--- gmi/urls.py
+++ gmi/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^scooper/', include('gmi.scooper.urls', namespace='gmi.scooper')),
+    url(r'^scooper/', include('gmi.django.scooper.urls', namespace='scooper')),
+    url(r'^profile/', include('gmi.django.profile.urls', namespace='profile')),
     url(r'^admin/', include(admin.site.urls)),
 ]
