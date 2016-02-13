9ba0e5add6a03685fadf4eb4ed49a829190f5ab5
Stefan <stefan@neuland.io>
2016-02-13 19:37:45 +0100

    fix overlapping module namespaces, include gunicorn

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', include('gmi.crewsite.urls', namespace='crewsite')),
+    url(r'^$', include('app.crewsite.urls', namespace='crewsite')),
     url(r'^scooper/', include('gmi.django.scooper.urls', namespace='scooper')),
     url(r'^profile/', include('gmi.django.profile.urls', namespace='profile')),
     url(r'^admin/', include(admin.site.urls)),
